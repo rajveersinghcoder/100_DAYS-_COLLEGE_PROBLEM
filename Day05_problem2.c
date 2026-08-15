6e0e7df80b952363629f4c5e19d8e8553f873df8
rajveersinghcoder <rajveerrs1445@gmail.com>
2026-08-15 22:02:53 +0530

    Updated Day 05 problem 2

--- Day05_problem2.c
+++ Day05_problem2.c
@@ -1,16 +1,22 @@
- //Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+ //utes:seconds formatWrite a program to input time in seconds and convert it to hours:min.
 
  #include<stdio.h>
 
  int main(){
 
-    int H;
-    int M;
-    int S;
+    int H; //Hours
+    int M; //Minutes
+    int S; //Seconds
+    int R; // Remaining
 
-    
+    printf("Enter time in Seconds :");
+    scanf("%d" ,&S);
 
+    H = S / 3600;
+    R = S % 3600;
+    M = R / 60;
 
+    printf("Time = %d:%02d\n", H, M);
 
     return 0;
  }
